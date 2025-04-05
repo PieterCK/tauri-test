b190aa75e1d0bb847064118f0cb2b1369025ef2f
PieterCK <pieterceka123@gmail.com>
2025-04-05 16:21:30 +0700

    typed_ipc: Add typed invoke command.

---
+++ web/src/typed_ipc.ts
@@ -0,0 +1,19 @@
+import { invoke as invokeTauri } from "@tauri-apps/api/core";
+import { ServerConfig } from "./server_settings";
+
+interface invokeAttr<A, R> {
+  args: A;
+  returns: R;
+}
+
+interface invokeCommands {
+  // src/server_settings.rs/get_server_settings()
+  "get_server_settings": invokeAttr<Record<string, string>, ServerConfig>;
+}
+
+export function invoke<T extends keyof invokeCommands>(
+  command: T,
+  args: invokeCommands[T]["args"]
+): Promise<invokeCommands[T]["returns"]> {
+  return invokeTauri(command, args);
+}
